56942b01ae55a47c0aada6b899e789a80ef05e9a
marc <Marc.hoogendoorn@live.nl>
2021-01-14 20:08:40 +0100

    Functie voor

---
+++ 6-1-2021-RequestinAD.ps1
@@ -0,0 +1,14 @@
+<#
+Script Idee: 
+
+Kijken in DNS naar alle mogelijke. Active Directories 
+
+Vervolgens naar allemaal een request sturen 
+
+Kijken welke het snelst reageert 
+
+En Die active Directory Gebruiken voor het draaien van het Script 
+
+NOU DAS NOU TOCH IS MOOI HE ALS DA WERKT. LOl
+
+#>
